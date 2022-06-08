336e8190ae54fe6e62d7ebd772c3bf5564c8fc3b
彭喆 <pengzhechina@sina.com>
2022-06-08 21:35:54 +0800

    url取参数

--- src/view/home.vue
+++ src/view/home.vue
@@ -3,10 +3,28 @@
         <router-link to='/figureNodeList'>节点图</router-link><br/>
         <div class="row"></div>
         <router-link to='/calculation'>计算器</router-link>
+        <div style="margin-top: 20px">
+            <button @click='urlParameterFn("status")'>url取参数</button>
+        </div>   
     </div>
 </template>
 <script>
 export default {
+    methods:{
+        urlParameterFn(name) {
+            const urlReg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+            let parameter = ""
+            if (window.location.search) {
+                parameter = window.location.search.substr(1).match(urlReg); 
+            } else {
+                parameter = window.location.hash.split("?")[1].match(urlReg)
+            }
+            if (parameter[2]) {
+               console.log(unescape(parameter[2]));
+               return unescape(parameter[2])
+            }
+        }
+    }
     
 }
 </script>
